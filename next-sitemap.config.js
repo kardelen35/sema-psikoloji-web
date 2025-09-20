9fb612ca741595215a5a35a4ce00dc58eed8688c
kardelen35 <kardelen62simsek@gmail.com>
2025-09-20 10:09:45 +0300

    Added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://aliagapsikologum.com',
+    generateRobotsTxt: true,
+  };
+  
+  export default config;
+  
